d0a2d460bf6719dcb0cb7cb0ce9d4a01081ce767
Joseph87555 <jaeshen1023@icloud.com>
2025-07-18 12:03:13 +0800

    Update python_project.py

--- python_project.py
+++ python_project.py
@@ -25,9 +25,36 @@ def rear():
     tom.right(31)
     tom.circle(-160,-10)
 
+    tom.
+import turtle
+tom=turtle.Turtle()
+color=("green")
+
+def tyre():
+    tom.setheading(0)
+
+    tom.circle(25)
+
+def move(x,y):
+    tom.penup()
+    tom.goto(x,y)
+    tom.pendown()
+
+def rear():
+    tom.right(32)
+    tom.circle(100,-100)
+    tom.right(25)
+    tom.circle(300,-100)
+
+    tom.right(50)
+    tom.circle(221,-20)
+
+    tom.right(31)
+    tom.circle(-160,-10)
+
     tom.left(31)
     tom.forward(200)
-'''    
+
 
 move(-150,-100)
 tyre()
@@ -37,27 +64,29 @@ move(100,-100)
 tyre()
 
 
-move(-120,-95)
+move(-120,-80)
 tom.fd(200)
-'''
-move(-180,-95)
+
+move(-180,-90)
 rear()
 
-'''
+
 tom.penup()
 tom.bk(140)
 tom.pendown()
 tom.forward(140)
+
 tom.penup()
 tom.bk(140)
 tom.pendown()
-tom.left(160)
-#start for rear
+tom.left(150)
 tom.penup()
 tom.bk(70)
+tom.penup()
+#Start from rear
 tom.forward(90)
 tom.pendown()
-tom.right(45)
+tom.right(170)
 tom.forward(50)
 tom.right(41)
 tom.forward(50)
@@ -71,10 +100,11 @@ tom.forward(50)
 tom.right(170)
 tom.forward(100)
 tom.penup()
-'''
 
 #done//
 turtle.done()
+
+
 
 '''
 jyjhyrjhy
