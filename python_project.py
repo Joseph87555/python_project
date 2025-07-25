47778a46991785aa68f125efaab3475992f44aa3
Joseph87555 <jaeshen1023@icloud.com>
2025-07-25 17:59:49 +0800

    Update python_project.py

--- python_project.py
+++ python_project.py
@@ -1,31 +1,4 @@
-
-import turtle
-tom=turtle.Turtle()
-color=("green")
-
-def tyre():
-    tom.setheading(0)
-
-    tom.circle(25)
-
-def move(x,y):
-    tom.penup()
-    tom.goto(x,y)
-    tom.pendown()
-
-def rear():
-    tom.right(31)
-    tom.circle(100,-20)
-    tom.right(20)
-    tom.circle(300,-10)
-
-    tom.right(50)
-    tom.circle(220,-20)
-
-    tom.right(31)
-    tom.circle(-160,-10)
-
-    tom.
+    
 import turtle
 tom=turtle.Turtle()
 color=("green")
@@ -53,23 +26,16 @@ def rear():
     tom.circle(-160,-10)
 
     tom.left(31)
-    tom.forward(200)
-
-
-move(-150,-100)
+    tom.forward(20)
+move(-120,-100)
 tyre()
-
 move(100,-100)
-
 tyre()
-
-
-move(-120,-80)
+move(-120,-110)
 tom.fd(200)
 
-move(-180,-90)
+move(-100,-45)
 rear()
-
 
 tom.penup()
 tom.bk(140)
@@ -86,25 +52,34 @@ tom.penup()
 #Start from rear
 tom.forward(90)
 tom.pendown()
-tom.right(170)
+tom.right(120)
 tom.forward(50)
 tom.right(41)
-tom.forward(50)
+tom.forward(50) 
 tom.right(80)
-tom.forward(150)
+tom.forward(110)
 tom.forward(90)
 tom.right(90)
 tom.forward(50)
 tom.left(90)
 tom.forward(50)
-tom.right(170)
-tom.forward(100)
+tom.right(90)
+tom.forward(110)
+#Stop on the front 
 tom.penup()
+
+tom.penup()
+tom.bk(200)
+tom.pd()
+tom.right(90)
+tom.forward(150)
+tom.bk(100)
+tom.right(90)
+tom.forward(40)
+
+
+
+
 
 #done//
 turtle.done()
-
-
-
-'''
-jyjhyrjhy
