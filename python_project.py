988bbfbbdb421f88772335dd78dab8876dd742c1
Joseph87555 <jaeshen1023@icloud.com>
2025-09-10 15:59:24 +0800

    Update python_project.py

--- python_project.py
+++ python_project.py
@@ -1,52 +1,100 @@
-    
+=
 def tom():
     tom=turtle
+    tom.colot("red")
     tom.circle(25)
+
+
 def tyre():
     tom.setheading(0)
     tom.circle(20)
-def move(x,y):
+
+
+def move():
     tom.penup()
     tom.goto(x,y)
     tom.pendown()
+    tom.bk (100)
+    tom.right(45)
+    tom.forward(100)
+
 def rear():
     tom.right(32)
     tom.right(30)
-    tom.circle(-10,-100)
-    tom.right(25)
+    tom.circle(-100,-100)
+    tom.right(20)
     tom.circle(200,-10)
     tom.circle(200,-100)
-    tom.right(50)
-    tom.circle(200,-100)
-    tom.right(52)
-    tom.circle(200,-100)
-    tom.right(30)
-    tom.circle(-160,-10)
+    tom.right(20)
+    tom.circle(20,-10)
+    tom.right(40)
+    tom.circle(20,-100)
+    tom.right(10)
+    tom.circle(-160,-100)
     tom.circle(-100,-10)
-    tom.left(31)
+    tom.left(20)
     tom.forward(20)
-move(-120,-100)
-tom.fd(200)
-move(-100,-100)
-rear():
-move(-100,100)
-tyre()
-move(100,-100)
-tyre()
-move(-120,-100)
-tom.fd(200)
-move(-100,-100)
-rear()
-tom.penup()
-tom.pendown()
-tom.bk(200)
-tom.bk(100)
-tom.pd()
+    move(-20,-100)
+    tom.fd(100)
+    move(-100,-100)
+
+    rear()
+    move(-100,100)
+    tyre()
+    move(100,-100)
+    tyre()
+    move(-100,-20)
+    tom.fd(200)
+    move(-100,-100)
+    rear()
+    tom.penup()
+    tom.pendown()
+    tom.bk(200)
+    tom.bk(100)
+    tom.pendown()
+    tom.right(40)
+    tom.forward(150)
+    tom.forward(120)
+    tom.bk(100)
+    tom.right(90)
+    tom.forward(40)
+    #done//
+    turtle.done()
+
+
+def spolier():
+    tom.forward(100)
+    tom.left(45)
+    tom.right(90)
+    tom.forward(100)
+    tom.right(90)
+    tom.forward(100)
+    tom.right(90)
+    tom.forward(200)
+    tom.right(90)
+    tom.forward(200)
+    
+def window plannel():
+tom forward(100)
 tom.right(90)
-tom.forward(150)
-tom.forward(120)
-tom.bk(100)
+tom.forward(20)
 tom.right(90)
-tom.forward(40)
-#done//
-turtle.done()
+tom.forward(100)
+tom.right(90)
+tom.forward(100)
+tom.right(90)
+tom.forward(10)
+tom.right(90)
+tom.forward(100)
+
+
+def air vent():
+tom.right(100)
+tom.right(90)
+tom.forward(200)
+tom.right(90)
+tom.forward(100)
+tom.right(90)
+tom.forward(200)
+
+
